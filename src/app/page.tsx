c9112f2c96e1b6bcc8b913512890b056ec7ab5c3
BrazilianBeast <lucaslucal75@gmail.com>
2024-08-05 21:48:53 -0300

    feat: add header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
-export default function Home() {
-    return <Button>Text</Button>
+const Home = async () => {
+    return (
+        <div>
+            <Header />
+            <div className="p-5">
+                <h2 className="text-xl font-bold">Olá, Lucas!</h2>
+                <p>Segunda-feira, 05 de agosto.</p>
+
+                <div className="mt-6 flex place-items-center gap-2">
+                    <Input placeholder="Faça sua busca..." />
+                    <Button>
+                        <SearchIcon />
+                    </Button>
+                </div>
+            </div>
+            <div className="relative mt-6 h-[150px] w-full">
+                <Image
+                    className="rounded-xl object-cover"
+                    alt="Agende nos melhores com FSW Barber"
+                    src="/banner-01.png"
+                    fill
+                />
+            </div>
+        </div>
+    )
 }
+
+export default Home
