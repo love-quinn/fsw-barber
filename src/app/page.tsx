7bc19c6ff45d754976b74701d925ee4b6721e9ad
BrazilianBeast <lucaslucal75@gmail.com>
2024-08-16 00:47:28 -0300

    feat: add welcome dynamic message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
     const session = await getServerSession(authOptions)
@@ -43,8 +45,18 @@ const Home = async () => {
         <div>
             <Header />
             <div className="p-5">
-                <h2 className="text-xl font-bold">Olá, Lucas!</h2>
-                <p>Segunda-feira, 05 de agosto.</p>
+                <h2 className="text-xl font-bold">
+                    Olá, {session?.user ? session.user.name : "bem vindo"}!
+                </h2>
+                <p>
+                    <span className="capitalize">
+                        {format(new Date(), "EEEE, dd", { locale: ptBR })}
+                    </span>
+                    <span>&nbsp;de&nbsp;</span>
+                    <span className="capitalize">
+                        {format(new Date(), "MMMM", { locale: ptBR })}
+                    </span>
+                </p>
 
                 {/* Busca */}
                 <div className="mt-6">
